28da5a7bfd89e91daa16e91cc0fd3af2c79981a1
manu12perez <manu12perez@gmail.com>
2024-10-11 09:46:44 +0200

    SumarNumeros

--- src/components/SumarNumeros/SumarNumeros.js
+++ src/components/SumarNumeros/SumarNumeros.js
@@ -1,20 +1,20 @@
 import imagenjoker from "../../assets/images/joker.jpg";
 import "./SumarNumeros.css";
 
-function SumarNumeros() {
+function SumarNumeros(props) {
   const suma = (numero1, numero2) => {
-    var resultado = numero1 + numero2;
-    console.log("Suma " + resultado);
+    var resultado = parseInt(props.numero1) + parseInt(props.numero2);
+    console.log("Suma = " + resultado);
   };
 
   return (
     <div>
-        <h1 style={{color:"blue"}}>Sumar números</h1>
+      <h1 style={{ color: "blue" }}>Sumar números</h1>
       <img src={imagenjoker} alt="imagen" />
       <br />
-      <button onClick={() => suma(8, 10)}>Sumar 8 + 10</button>
-      <br />
-      <button onClick={() => suma(15, 32)}>Sumar 15 + 32</button>
+      <button onClick={() => suma()}>
+        Sumar {props.numero1}+{props.numero2}
+      </button>
       <br />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import SumarNumeros from './components/SumarNumeros/SumarNumeros';
-import App from './components/App/App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import SumarNumeros from "./components/SumarNumeros/SumarNumeros";
+import App from "./components/App/App";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <SumarNumeros />
+    <SumarNumeros numero1="5" numero2="3" />
+    <SumarNumeros numero1="84" numero2="116" />
   </React.StrictMode>
 );
 
